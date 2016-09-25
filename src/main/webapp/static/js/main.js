81cc86e1c0bca4299d2b80e6f973e0eb9cb4ad20
nihao17546 <420584320@qq.com>
2016-09-25 22:03:29 +0800

    js

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,5 +1,70 @@
+(function($) {
+	$.fn.serializeJson = function() {
+		var serializeObj = {};
+		$(this.serializeArray()).each(function() {
+			serializeObj[this.name] = this.value;
+		});
+		return serializeObj;
+	};
+	
+	$.fn.validate = function() {
+		var check = true;
+		var valiTags = $(this).find('.need-validate');
+		for (var i = 0, len = valiTags.length; i < len; i++) {
+			var valiStrs = $(valiTags[i]).attr('validation');
+			if (typeof (valiStrs) != 'undefined') {
+				var valiStr = valiStrs.split(',');
+				var message = '';
+				for (var j = 0, lenj = valiStr.length; j < lenj; j++) {
+					switch (valiStr[j]) {
+					case 'required':
+						if ($.trim($(valiTags[i]).val()) == '') {
+							message = message + '必输项!';
+						}
+						break;
+					case 'cellphone':
+						var reg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
+						if ($(valiTags[i]).val() != ''
+								&& !reg.test($(valiTags[i]).val())) {
+							message = message + '手机号码格式有误!';
+						}
+						break;
+					default:
+						break;
+					}
+				}
+				if (message != '') {
+					$(valiTags[i]).attr('title', message);
+					$(valiTags[i]).parent().removeClass('has-success');
+					$(valiTags[i]).parent().addClass('has-error');
+					$(valiTags[i]).tooltip('show');
+					check = false;
+				} else {
+					$(valiTags[i]).attr('title', '');
+					$(valiTags[i]).parent().removeClass('has-error');
+					$(valiTags[i]).parent().addClass('has-success');
+					$(valiTags[i]).tooltip('destroy');
+				}
+			}
+		}
+		return check;
+	};
+})(jQuery);
+
+$(function() {
+	 $(".need-validate").bind("input propertychange", function() {
+		 if($(this).attr('title')!=''){
+			 $(this).attr('title', '');
+			 $(this).parent().removeClass('has-error');
+			 $(this).parent().addClass('has-success');
+			 $(this).tooltip('destroy');
+		 }
+	 });
+})
+
 /**
  * 获取contextPath
+ * 
  * @returns
  */
 function getContextPath() {
@@ -9,37 +74,36 @@ function getContextPath() {
 	return result;
 }
 
-function refreshTable(){
+function refreshTable() {
 	var data_table = $("#data_table");
 	data_table.bootstrapTable("refresh");
 }
 
-function GetQueryString(name)
-{
-     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
-     var r = window.location.search.substr(1).match(reg);
-     if(r!=null)return  unescape(r[2]); return null;
+function GetQueryString(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if (r != null)
+		return unescape(r[2]);
+	return null;
 }
 
-function initForm(param,tag)
-{
-     $.ajax({
-    	type:'post',
-    	url:param.url,
-    	data:param.data,
-    	dataType:'json',
-    	success:function(data){
-    		if(data.code==403){
-    			alert(data.message);
-    		}
-    		else{
-    			for(var key in data.data){
-    				$("#"+key).val(data.data[key]);
-    			}
-    		}
-    	},
-    	error:function(){
-    		alert("异常!");
-    	}
-     });
+function initForm(param, tag) {
+	$.ajax({
+		type : 'post',
+		url : param.url,
+		data : param.data,
+		dataType : 'json',
+		success : function(data) {
+			if (data.code == 403) {
+				alert(data.message);
+			} else {
+				for ( var key in data.data) {
+					$("#" + key).val(data.data[key]);
+				}
+			}
+		},
+		error : function() {
+			alert("异常!");
+		}
+	});
 }
